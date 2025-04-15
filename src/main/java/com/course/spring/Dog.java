6bba0b86aeaade1d67df3de03cf23a3698b53dc5
remrtehno <remrtehno@gmail.com>
2025-04-15 17:48:27 +0200

    S1(20): LifeCycle, PreDestroy, PostController

--- src/main/java/com/course/spring/Dog.java
+++ src/main/java/com/course/spring/Dog.java
@@ -3,12 +3,25 @@ package com.course.spring;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component("myDog")
 //@Scope("singleton")
-@Scope("prototype")
+//@Scope("prototype")
 public class Dog implements Pet{
     @Override
     public void say() {
         System.out.println("Say");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Dos class is created");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Dog is going to be destroyed");
     }
 }
