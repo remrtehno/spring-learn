9680d530a6c2a7e186869a8295366f8aa6f2b18d
remrtehno <remrtehno@gmail.com>
2025-04-15 17:17:11 +0200

    S1(14,15): Annotations @Component, @Autowired

--- src/main/java/com/course/spring/Dog.java
+++ src/main/java/com/course/spring/Dog.java
@@ -1,5 +1,8 @@
 package com.course.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component("myDog")
 public class Dog implements Pet{
     @Override
     public void say() {
--- src/main/java/com/course/spring/Person.java
+++ src/main/java/com/course/spring/Person.java
@@ -1,18 +1,23 @@
 package com.course.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("myPerson")
 public class Person {
     Pet pet;
-//    public Person(Pet pet) {
-//        this.pet = pet;
-//    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
+    @Autowired
+    public Person(Pet pet) {
         this.pet = pet;
     }
+
+//    public Pet getPet() {
+//        return pet;
+//    }
+//
+//    public void setPet(Pet pet) {
+//        this.pet = pet;
+//    }
 
     public void petYourPet(){
         System.out.println("Petting your pet: ");
