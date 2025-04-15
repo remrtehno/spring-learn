d4a0b35b1e2f0671bdcd2741f1a39db5d7c1c199
remrtehno <remrtehno@gmail.com>
2025-04-15 18:32:40 +0200

    S1(21): Config without XML

---
+++ src/main/java/com/course/spring/MyConfig.java
@@ -0,0 +1,10 @@
+package com.course.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.course.spring")
+public class MyConfig {
+
+}
--- src/main/java/com/course/spring/Test.java
+++ src/main/java/com/course/spring/Test.java
@@ -1,15 +1,18 @@
 package com.course.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 //        Person person = context.getBean("myPerson", Person.class);
 //        person.petYourPet();
 //
 //        System.out.println(person.name);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         Pet dog = context.getBean("myDog", Dog.class);
         Pet dog2 = context.getBean("myDog", Dog.class);
