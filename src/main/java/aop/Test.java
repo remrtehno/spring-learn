9f775d0de8d903768169b736f06a0658b2d937f7
remrtehno <remrtehno@gmail.com>
2025-04-16 18:36:58 +0200

    S2(24,25): @EnableAspectJAutoProxy, @Aspect,  @Before("execution(* *())")

---
+++ src/main/java/aop/Test.java
@@ -0,0 +1,15 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Library library = context.getBean("library", Library.class);
+        library.getBook();
+
+        context.close();
+    }
+}
