d5fb8c5b245098513926595ac05c79f6550215d8
remrtehno <remrtehno@gmail.com>
2025-04-23 19:04:31 +0200

    S4(57): Spring MVC first app

---
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -0,0 +1,16 @@
+package com.zaurtregulov.spring.mvc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "firstView";
+    }
+}
