60514df2f55c1bf2f60fdbd1e033117b28150e0f
remrtehno <remrtehno@gmail.com>
2025-04-26 19:56:51 +0200

    clean up

--- src/main/java/com/zaurtregulov/spring/rest/exception_handling/NoSuchEmployeeException.java
+++
@@ -1,7 +0,0 @@
-package com.zaurtregulov.spring.rest.exception_handling;
-
-public class NoSuchEmployeeException extends RuntimeException {
-    public NoSuchEmployeeException(String message) {
-        super(message);
-    }
-}
