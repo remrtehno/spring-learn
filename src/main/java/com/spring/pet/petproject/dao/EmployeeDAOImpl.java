0d9d0e36699dd5e3a9fe6e2c700eda7517e846c1
remrtehno <remrtehno@gmail.com>
2025-04-27 18:49:20 +0200

    S8(99): JPA Query; entityManager.find, entityManager.createQuery,  entityManager.merge

--- src/main/java/com/spring/pet/petproject/dao/EmployeeDAOImpl.java
+++ src/main/java/com/spring/pet/petproject/dao/EmployeeDAOImpl.java
@@ -3,8 +3,8 @@ package com.spring.pet.petproject.dao;
 
 import com.spring.pet.petproject.entity.Employee;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,34 +18,29 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
-        List<Employee>    employees = query.getResultList();
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> employees = query.getResultList();
 
         return employees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(employee);
+            Employee employee1 = entityManager.merge(employee);
+            employee.setId(employee1.getId()); //because ID = 0 when creates a new employee
     }
 
     @Override
     public Employee getEmployeeById(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
+
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-
-        Query<Employee> query = session.createQuery("delete from Employee where id=:id");
+        Query query = entityManager.createQuery("delete from Employee where id=:id");
         query.setParameter("id", id);
         query.executeUpdate();
-
     }
 }
